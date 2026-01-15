5fe977868a14964e46cd716b62ae74880ffe028f
d3f4l7d <idaten.bekko@gmail.com>
2026-01-15 17:58:16 +0900

    Update safearch.sh

--- safearch.sh
+++ safearch.sh
@@ -69,3 +69,11 @@ reboot
 sudo pacman -S openssh ufw xorg 
 sudo systemctl start sshd && sudo systemctl start ufw
 sudo systemctl enable sshd && sudo systemctl enable ufw
+
+echo
+echo " _   _      _ _        __        __         _     _ _ "
+echo "| | | | ___| | | ___   \ \      / /__  _ __| | __| | |"
+echo "| |_| |/ _ \ | |/ _ \   \ \ /\ / / _ \| '__| |/ _` | |"
+echo "|  _  |  __/ | | (_) |   \ V  V / (_) | |  | | (_| |_|"
+echo "|_| |_|\___|_|_|\___/     \_/\_/ \___/|_|  |_|\__,_(_)"
+echo
