a9ba6b764b1675de836e9d7599df1716edf9edfa
d3f4l7d <idaten.bekko@gmail.com>
2026-01-15 17:52:07 +0900

    Update safearch.sh

--- safearch.sh
+++ safearch.sh
@@ -7,6 +7,63 @@ echo "|____/_/   \_\_|   |_____/_/   \_\_| \_\\____|_| |_|"
 echo
 
 timedatectl set-ntp true
-cfdisk /dev/nvme0n1
-echo "select gpt"
-echo ""
+fdisk /dev/nvme0n1
+echo "g"
+echo "n"
+echo "1 (default)"
+echo "2048 (default)"
+echo "+500M"
+echo "t"
+echo "L"
+echo "1"
+echo "n"
+echo "2 (Default)"
+echo "1026048 (default)"
+echo "209715166 (default)"
+echo "w"
+mkfs.fat -F32 /dev/nvme0n1p1
+mkfs.ext4 /dev/nvme0n1p2
+mount /dev/nvme0n1p2 /mnt
+mkdir /mnt/boot
+mount /dev/nvme0n1p1 /mnt/boot
+pacstrap /mnt base linux linux-firmware vim
+genfstab -U /mnt >> /mnt/etc/fstab
+arch-chroot /mnt
+fallocate -l 2GB /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+echo "/swapfile none swap defaults 0 0" >> /etc/fstab
+ln -sf /usr/share/zoneinfo/Japan /etc/localtime
+hwclock --systohc
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+echo "ja_JP.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "n4m3h3r3p134z" >> /etc/hostname
+echo "127.0.0.1        localhost" >> /etc/hosts
+echo "::1              localhost" >> /etc/hosts
+echo "127.0.1.1        n4m3h3r3p134z.localdomain        n4m3h3r3p134z" >> /etc/hosts
+passwd
+pacman -S efibootmgr networkmanager network-manager-applet wireless_tools wpa_supplicant dialog os-prober mtools dosfstools base-devel linux-headers
+bootctl --path=/boot install
+cd /boot
+cd loader/
+echo "timeout 3" >> /boot/loader/loader.conf
+echo "default n4m3h3r3p134z-*" >> /boot/loader/loader.conf
+cd entries/
+echo "title    Arch Linux" >> /boot/loader/entries/n4m3h3r3p134z.conf
+echo "linux    /vmlinuz-linux" >> /boot/loader/entries/n4m3h3r3p134z.conf
+echo "initrd    /initramfs-linux.img" >> /boot/loader/entries/n4m3h3r3p134z.conf
+echo "options    root=/dev/nvme0n1p2 rw" >> /boot/loader/entries/n4m3h3r3p134z.conf
+cd
+systemctl enable NetworkManager
+useradd -mG wheel d3f4l7d
+passwd d3f4l7d
+EDITOR=vim visudo
+exit
+umount -a
+reboot
+
+sudo pacman -S openssh ufw xorg 
+sudo systemctl start sshd && sudo systemctl start ufw
