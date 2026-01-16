efbff5e186845b2a09987873ec8d0ffde2b69a6c
d3f4l7d <idaten.bekko@gmail.com>
2026-01-16 17:09:54 +0900

    Update safearch.sh

--- safearch.sh
+++ safearch.sh
@@ -63,7 +63,7 @@ exit
 umount -a
 reboot
 
-sudo pacman -S openssh ufw xorg 
+sudo pacman -S openssh ufw xorg git
 sudo systemctl start sshd && sudo systemctl start ufw
 sudo systemctl enable sshd && sudo systemctl enable ufw
 
