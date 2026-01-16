50f258d6f9e3e445cb04b927bcca3a0a90a866e9
d3f4l7d <idaten.bekko@gmail.com>
2026-01-16 16:34:07 +0900

    Update safearch.sh

--- safearch.sh
+++ safearch.sh
@@ -26,7 +26,7 @@ mkfs.ext4 /dev/nvme0n1p2
 mount /dev/nvme0n1p2 /mnt
 mkdir /mnt/boot
 mount /dev/nvme0n1p1 /mnt/boot
-pacstrap /mnt base linux linux-firmware vim linux-lts linux-lts-headers archlinux-keyring
+pacstrap /mnt base linux linux-headers linux-firmware vim linux-lts linux-lts-headers archlinux-keyring
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
 fallocate -l 2GB /swapfile
