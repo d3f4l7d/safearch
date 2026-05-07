7715163cac416175a1da1293c32b346c63c2f3c4
d3f4l7d <idaten.bekko@gmail.com>
2026-05-07 09:58:23 +0900

    Update safearch.sh

--- safearch.sh
+++ safearch.sh
@@ -88,15 +88,3 @@ umount -a
 reboot
 
 echo "Enter BIOS and find Arch Linux on Boot menu or point out /efi/boot/bootx64.efi or /grub/x86_64-efi/grub.efi instead"
-
-sudo pacman -S openssh ufw xorg git
-sudo systemctl start sshd && sudo systemctl start ufw
-sudo systemctl enable sshd && sudo systemctl enable ufw
-
-echo
-echo " _   _      _ _        __        __         _     _ _ "
-echo "| | | | ___| | | ___   \ \      / /__  _ __| | __| | |"
-echo "| |_| |/ _ \ | |/ _ \   \ \ /\ / / _ \| '__| |/ _' | |"
-echo "|  _  |  __/ | | (_) |   \ V  V / (_) | |  | | (_| |_|"
-echo "|_| |_|\___|_|_|\___/     \_/\_/ \___/|_|  |_|\__,_(_)"
-echo
