a3f6b3868d41e81aa3bfdcf8f1b865de0d40172e
d3f4l7d <idaten.bekko@gmail.com>
2026-05-07 10:00:33 +0900

    Update helloworld.sh

--- helloworld.sh
+++ helloworld.sh
@@ -1,3 +1,21 @@
-sudo pacman -S openssh ufw xorg git
+echo
+echo " _   _      _ _        __        __         _     _ _ "
+echo "| | | | ___| | | ___   \ \      / /__  _ __| | __| | |"
+echo "| |_| |/ _ \ | |/ _ \   \ \ /\ / / _ \| '__| |/ _' | |"
+echo "|  _  |  __/ | | (_) |   \ V  V / (_) | |  | | (_| |_|"
+echo "|_| |_|\___|_|_|\___/     \_/\_/ \___/|_|  |_|\__,_(_)"
+echo
+
+
+sudo pacman -S --noconfirm openssh ufw xorg git
 sudo systemctl start sshd && sudo systemctl start ufw
 sudo systemctl enable sshd && sudo systemctl enable ufw
+
+
+echo
+echo " _   _      _ _        __        __         _     _ _ "
+echo "| | | | ___| | | ___   \ \      / /__  _ __| | __| | |"
+echo "| |_| |/ _ \ | |/ _ \   \ \ /\ / / _ \| '__| |/ _' | |"
+echo "|  _  |  __/ | | (_) |   \ V  V / (_) | |  | | (_| |_|"
+echo "|_| |_|\___|_|_|\___/     \_/\_/ \___/|_|  |_|\__,_(_)"
+echo
